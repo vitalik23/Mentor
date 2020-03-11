cf13d71d6c39e5a8f57d4db10714389403fe732a
dudchenko610 <rusland610@.gmail.com>
2020-03-11 13:15:23 +0200

    Added student controller

--- Mentor/Controllers/StudentController.cs
+++ Mentor/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace Mentor.Controllers
 {
+
+    [Authorize(Roles = RoleInitializer.ROLE_STUDENT)]
     public class StudentController : Controller
     {
 
