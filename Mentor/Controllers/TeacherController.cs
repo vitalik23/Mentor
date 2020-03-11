4185c8b4f03ec0b9bf29312cd2c7d0f5b01a8d0f
vitalik23 <w.basanets2301@gmail.com>
2020-03-11 12:23:12 +0200

    Added page TeacherProfile

---
+++ Mentor/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mentor.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult TeacherProfile()
+        {
+            return View();
+        }
+
+
+    }
+}
