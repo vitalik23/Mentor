d43fbef107fc2e6d34d0487ad4c9ad726ca84917
dudchenko610 <rusland610@.gmail.com>
2020-03-23 14:47:51 +0200

    Chat system was started

---
+++ Mentor/Models/Chat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mentor.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string User1Id { get; set; }
+        public string User2Id { get; set; }
+
+        public string HistoryPath { get; set; }
+
+        public virtual User User1 { get; set; }
+        public virtual User User2 { get; set; }
+
+    }
+}
